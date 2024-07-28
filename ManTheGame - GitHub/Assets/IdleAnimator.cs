2114945ba40d9629e417dca1b25b6727b2c8104c
ManTheGameLss <manthegamelss@gmail.com>
2024-07-28 21:29:00 +0200

    made functioning bools
    i finished all of the bools in the script for the 2d character

--- ManTheGame - GitHub/Assets/IdleAnimator.cs
+++ ManTheGame - GitHub/Assets/IdleAnimator.cs
@@ -9,13 +9,13 @@ public class IdleAnimator : MonoBehaviour
     public Animator animator;
 
     public bool frontSprite;
-    bool frontSideRight;
-    bool frontSideLeft;
-    bool sideRight;
-    bool sideLeft;
-    bool behindSideRight;
-    bool behindSideLeft;
-    bool behindSprite;
+    public bool frontSideRight;
+    public bool frontSideLeft;
+    public bool sideRight;
+    public bool sideLeft;
+    public bool behindSideRight;
+    public bool behindSideLeft;
+    public bool behindSprite;
 
     public float generalAngle;
    
@@ -142,7 +142,7 @@ public class IdleAnimator : MonoBehaviour
 
         #region Example
         //in this case, if generalAngle is a number between 23 and -23 it meens that you're watching the sprite from the front and renders the front-sprite
-        if ((generalAngle < 24 && generalAngle > 0) || (generalAngle < 360 && generalAngle > 336))
+        if ((generalAngle < 22.5 && generalAngle > 0) || (generalAngle < 360 && generalAngle > 337.5))
         {
             frontSprite = true;
             CheckSpriteBools();
@@ -151,15 +151,106 @@ public class IdleAnimator : MonoBehaviour
         {
             frontSprite = false;
         }
+        
         #endregion
+
+        #endregion
+
+        #region frontSideRight
+
+        if (generalAngle < 337.5 && generalAngle > 292.5)
+        {
+            frontSideRight = true;
+            CheckSpriteBools();
+        }
+        else
+        {
+            frontSideRight = false;
+        }
+
+        #endregion
+
+        #region SideRight
+
+        if (generalAngle < 292.5 && generalAngle > 247.5)
+        {
+            sideRight = true;
+            CheckSpriteBools();
+        }
+        else
+        {
+            sideRight = false;
+        }
+
+        #endregion
+
+        #region behindSideRight
+
+        if (generalAngle < 247.5 && generalAngle > 202.5)
+        {
+            behindSideRight = true;
+            CheckSpriteBools();
+        }
+        else
+        {
+            behindSideRight = false;
+        }
 
         #endregion
 
         #region Behind
 
+        if (generalAngle < 202.5 && generalAngle > 157.5)
+        {
+            behindSprite = true;
+            CheckSpriteBools();
+        }
+        else
+        {
+            behindSprite = false;
+        }
+
         #endregion
 
-        #region SideRight
+        #region behindSideLeft
+
+        if (generalAngle < 157.5 && generalAngle > 112.5)
+        {
+            behindSideLeft = true;
+            CheckSpriteBools();
+        }
+        else
+        {
+            behindSideLeft = false;
+        }
+
+        #endregion
+
+        #region sideLeft
+
+        if (generalAngle < 112.5 && generalAngle > 67.5)
+        {
+            sideLeft = true;
+            CheckSpriteBools();
+        }
+        else
+        {
+            sideLeft = false;
+        }
+
+        #endregion
+
+        #region frontSideLeft
+
+        if (generalAngle < 67.5 && generalAngle > 22.5)
+        {
+            frontSideLeft = true;
+            CheckSpriteBools();
+        }
+        else
+        {
+            frontSideLeft = false;
+        }
 
         #endregion
 
